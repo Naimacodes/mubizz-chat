8823bfe52b61b6fc31e195fe1c85216f2f4babcf
Rory Gilmore <naima.adanahmed@gmail.com>
2020-04-01 13:46:38 +0200

    completed joinChat component, added its styles

--- server/server.js
+++ server/server.js
@@ -10,5 +10,17 @@ const app = express()
 const server = http.createServer(app)
 const io = socketio(server)
 
+
+io.on('connection', (socket) => {
+console.log('we have a new connection');
+
+socket.on('disconnect', () =>{
+  console.log('user has left');
+})
+})
+
+
+
+
 app.use(router);
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
