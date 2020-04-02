259b1e1b1bcccded530b705aec1afd4bea36ddb6
Rory Gilmore <naima.adanahmed@gmail.com>
2020-04-02 11:36:36 +0200

    did basic setup for user joining and leaving

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,15 +1,30 @@
 import React, { useEffect, useState } from 'react';
 import Qs from 'query-string';
 import io from 'socket.io-client';
+import './Chat.css';
 
 const Chat = ({ location }) => {
   const [username, setName] = useState('');
   const [room, setRoom] = useState('');
+  const endpoint = 'localhost:5000';
+
+  let socket;
   useEffect(() => {
-    const { name, room } = Qs.parse(location.search);
-    setName(namez);
+    const { name, room } = Qs.parse(location.search, {
+      ignoreQueryPrefix: true
+    });
+    setName(name);
     setRoom(room);
-  });
+    console.log(name, room);
+    socket = io(endpoint);
+
+    socket.emit('join', { name, room }, () => {
+      
+      return () => {
+        socket.emit('disconnect');
+        socket.off()      }
+    });
+  }, [endpoint, location.search]);
 
   return <div>Chat</div>;
 };
