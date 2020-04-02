91671d0597e5e837f76d18de7d8689c4888e15ca
Rory Gilmore <naima.adanahmed@gmail.com>
2020-04-02 22:01:35 +0200

    the error was because I forgot to write socket.join(user.room)

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
 
-const { addUser, getUser, removeUser, getUsersInRoom } = require('./users.js');
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
 
 const router = require('./router');
 
@@ -16,23 +16,30 @@ io.on('connection', socket => {
     const { error, user } = addUser({ id: socket.id, name, room });
     if (error) return callback(error);
 
+    socket.join(user.room);
+
     /////////this one right here emits to the front end ADMIN generated messages
     socket.emit('message', {
       user: 'admin',
       text: `${user.name}, welcome to room ${user.room}.`
     });
-    socket.broadcast.to(user.room).emit('message', {
-      user: 'admin',
-      text: `${user.name} has joined the conversation`
-    });
+    socket.broadcast
+      .to(user.room)
+      .emit('message', {
+        user: 'admin',
+        text: `${user.name} has joined the conversation.`
+      });
 
     callback();
   });
 
   /////////we are expecting user generated messages
-  socket.on('sendMessage', message => {
+  socket.on('sendMessage', (message, callback) => {
     const user = getUser(socket.id);
+
     io.to(user.room).emit('message', { user: user.name, text: message });
+
+    callback();
   });
 
   socket.on('disconnect', () => {
@@ -40,5 +47,6 @@ io.on('connection', socket => {
   });
 });
 
-const PORT = 5000 || process.env.PORT;
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(process.env.PORT || 5000, () =>
+  console.log(`Server has started.`)
+);
