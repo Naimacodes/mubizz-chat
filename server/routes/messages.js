4b2b6827033bb75c3380d0546b5e0f93c9b1d8f4
Rory Gilmore <naima.adanahmed@gmail.com>
2020-04-09 12:04:33 +0200

    worked on messages route can now post, need to work on conversation update

--- server/routes/messages.js
+++ server/routes/messages.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const config = require('config');
 const auth = require('../middleware/auth');
 const { check, validationResult } = require('express-validator');
 const Message = require('../models/Message');
+
+
 
 router.get('/', auth, async (req, res) => {
   try {
@@ -19,61 +18,34 @@ router.get('/', auth, async (req, res) => {
   }
 });
 
+//@route api/messages
+//@desc add new messages
+//@access private
+router.post(
+  '/',
+  [auth, [check('body', 'You have to type something').not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { body } = req.body;
+    try {
+      const newMessage = new Message({
+        from: req.user.id,
+        to: req.body.to,
+        body,
+        date,
+      });
+      req.io.sockets.emit('messages', req.body.body);
+      const message = await newMessage.save();
+      res.json(message);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('server error');
+    }
+  }
+);
 
-router.post('/', (req, res) => {
-  let from = mongoose.Types.ObjectId(jwtUser.id);
-  let to = mongoose.Types.ObjectId(req.body.to);
-
-  Conversation.findOneAndUpdate(
-      {
-          recipients: {
-              $all: [
-                  { $elemMatch: { $eq: from } },
-                  { $elemMatch: { $eq: to } },
-              ],
-          },
-      },
-      {
-          recipients: [jwtUser.id, req.body.to],
-          lastMessage: req.body.body,
-          date: Date.now(),
-      },
-      { upsert: true, new: true, setDefaultsOnInsert: true },
-      function(err, conversation) {
-          if (err) {
-              console.log(err);
-              res.setHeader('Content-Type', 'application/json');
-              res.end(JSON.stringify({ message: 'Failure' }));
-              res.sendStatus(500);
-          } else {
-              let message = new Message({
-                  conversation: conversation._id,
-                  to: req.body.to,
-                  from: jwtUser.id,
-                  body: req.body.body,
-              });
-
-              req.io.sockets.emit('messages', req.body.body);
-
-              message.save(err => {
-                  if (err) {
-                      console.log(err);
-                      res.setHeader('Content-Type', 'application/json');
-                      res.end(JSON.stringify({ message: 'Failure' }));
-                      res.sendStatus(500);
-                  } else {
-                      res.setHeader('Content-Type', 'application/json');
-                      res.end(
-                          JSON.stringify({
-                              message: 'Success',
-                              conversationId: conversation._id,
-                          })
-                      );
-                  }
-              });
-          }
-      }
-  );
-});
 
 module.exports = router;
